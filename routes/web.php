d4247d2ecd6425002c049f4d2739baf021008a8f
Alex Alex <askon040@gmail.com>
2023-10-30 11:14:06 +0600

    fix 04

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,20 @@ Route::middleware('guest')->group(function () {
     Route::view('/login', 'auth.login')->name('adminLoginShow');
     Route::post('/login', [AdminAuthController::class, 'adminLoginForm'])->name('adminLoginForm');
 });
+//Clear route cache:
+ Route::get('/route-cache', function() {
+     $exitCode = Artisan::call('route:cache');
+     return 'Routes cache has been cleared';
+ });
+
+ //Clear config cache:
+ Route::get('/config-cache', function() {
+     $exitCode = Artisan::call('config:cache');
+     return 'Config cache has been cleared';
+ });
+
+ //Cache config cache:
+ Route::get('/cache-clear', function() {
+     $exitCode = Artisan::call('cache:clear');
+     return 'Application cache cleared';
+ });
